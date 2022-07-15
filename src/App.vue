bb9731429f657cd8c818d5d78b44d6495aef7c57
Denis <tudow40@gmail.com>
2022-07-15 15:30:14 +0300

    Added elevator shafts

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,85 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="app-wrapper">
+    <ul class="floor-dividers">
+      <li v-for="n in levels" :key="n" :style="{top: n * (100 / levels) + '%'}" class="floor-dividers__item"></li>
+    </ul>
+    <div class="elevator-shafts__wrapper">
+      <ul class="elevator-shafts">
+        <li v-for="n in elevatorsQuantity" :key="n" class="elevator-shafts__item"></li>
+      </ul>
+    </div>
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+<script setup lang="ts">
+const levels = 5;
+const elevatorsQuantity = 2;
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+
 </script>
 
 <style lang="scss">
+body {
+  margin: 0;
+}
+
 #app {
+  position: relative;
+  padding: 8px;
+  height: 1px;
+  min-height: 100vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+*, *:after, *:before {
+  box-sizing: border-box;
+}
+
+ul, li {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+.app-wrapper {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+
+.floor-dividers {
+  position: absolute;
+  z-index: -1;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  border-top: 2px solid #ececec;
+
+  &__item {
+    position: absolute;
+    height: 2px;
+    width: 100%;
+    background-color: #ececec;
+  }
+}
+
+.elevator-shafts {
+  display: flex;
+  padding-left: 10px;
+
+  &__wrapper {
+    display: flex;
+    height: 100%;
+  }
+
+  &__item {
+    width: 100px;
+    margin-right: 10px;
+    background-color: #42b983;
+  }
 }
 </style>
